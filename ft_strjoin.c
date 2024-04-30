bd57d6776129ba0cbfcd1345e5b3af1a04812a33
Yayoi-oo0 <154104141+Yayoi-oo0@users.noreply.github.com>
2024-04-30 13:24:58 +0900

    commit ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okamotoyayoi <okamotoyayoi@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 10:08:29 by okamotoyayo       #+#    #+#             */
+/*   Updated: 2024/04/30 13:04:40 by okamotoyayo      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	int		len1;
+	int		len2;
+	char	*str;
+	
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!str)
+		return (NULL);
+	*str = ft_stlrcat(*s1, *s2, len1+len2);
+	return (str);
+}

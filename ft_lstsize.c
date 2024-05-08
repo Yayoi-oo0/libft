1e7d338cc92b108eb86c3f3eff685068e847a429
Yayoi-oo0 <154104141+Yayoi-oo0@users.noreply.github.com>
2024-05-08 13:58:27 +0900

    lstsize

---
+++ ft_lstsize.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okamotoyayoi <okamotoyayoi@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 13:47:29 by okamotoyayo       #+#    #+#             */
+/*   Updated: 2024/05/08 13:53:47 by okamotoyayo      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_lstsize(t_list *lst)
+{
+	int		count;
+	t_list	*tmp;
+
+	count = 0;
+	tmp = lst;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+	return (count);
+}

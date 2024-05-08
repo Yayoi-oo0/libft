ee551266be6695ab2c0d783b2d86719b9246c836
Yayoi-oo0 <154104141+Yayoi-oo0@users.noreply.github.com>
2024-05-08 14:02:54 +0900

    lstlast

---
+++ ft_lstlast.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okamotoyayoi <okamotoyayoi@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 13:59:25 by okamotoyayo       #+#    #+#             */
+/*   Updated: 2024/05/08 14:02:35 by okamotoyayo      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst)
+	{
+		if (!lst->next)
+			return (lst);
+		lst = lst->next;
+	}
+	return (lst);
+}
